6b52c4031e0e40f52f589341607310405403e7db
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-02-28 20:24:19 +0000

    options in func

--- manual_rps.py
+++ manual_rps.py
@@ -1,15 +1,14 @@
 import random
 
-
-choices = ['rock', 'paper', 'scissors']
-
 def get_computer_choice():
-    computer_choice = random(choices)
+    options = ['rock', 'paper', 'scissors']
+    computer_choice = random(options)
     return computer_choice
 
 def get_user_choice():
+    options = ['rock', 'paper', 'scissors']
     user_input = input('rock, paper, scissors? ')
-    if user_input in choices:
+    if user_input in options:
         return user_input
     else:
         print('Must be rock, paper, or scissors')
