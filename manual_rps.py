10210f85431b37110ab3cd03a21df5d137517d71
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-02-28 20:22:18 +0000

    choice typo

--- manual_rps.py
+++ manual_rps.py
@@ -7,7 +7,7 @@ def get_computer_choice():
     computer_choice = random(choices)
     return computer_choice
 
-def get_user_coice():
+def get_user_choice():
     user_input = input('rock, paper, scissors? ')
     if user_input in choices:
         return user_input
