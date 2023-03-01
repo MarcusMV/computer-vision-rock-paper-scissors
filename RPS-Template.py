22ac9130b840fc75e43d3a291c25e29c2b4c8f96
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-03-01 20:07:47 +0000

    task 2 get_winner of game

--- RPS-Template.py
+++ RPS-Template.py
@@ -5,11 +5,12 @@ model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True: 
+while True:
     ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    resized_frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
     image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+    normalized_image = (image_np.astype(np.float32) /
+                        127.0) - 1  # Normalize the image
     data[0] = normalized_image
     prediction = model.predict(data)
     cv2.imshow('frame', frame)
@@ -17,7 +18,7 @@ while True:
     print(prediction)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-            
+
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
--- manual_rps.py
+++ manual_rps.py
@@ -5,8 +5,36 @@ def get_computer_choice():
     computer_choice = random.choice(options)
     return computer_choice
 
+
 def get_user_choice():
-    options = ['rock', 'paper', 'scissors']
     user_input = input('rock, paper, scissors? ')
     return user_input
-        
+
+
+def get_winner(computer_choice, user_choice):
+    comp = computer_choice.lower()
+    user = user_choice.lower()
+    if comp == 'rock':
+        if user == 'paper':
+            print('Paper beats rock, user wins')
+        elif user == 'scissors':
+            print('Rock beats scissors, computer wins')
+        else:
+            print('It\'s a draw')
+    elif comp == 'paper':
+        if user == 'rock':
+            print('Paper beats rock, computer wins')
+        elif user == 'scissors':
+            print('Scissors beats paper, user wins')
+        else:
+            print('It\'s a draw')
+    elif comp == 'scissors':
+        if user == 'rock':
+            print('Rock beats scissors, user wins')
+        elif user == 'paper':
+            print('Scissors beats paper, computer wins')
+        else:
+            print('It\'s a draw')
+
+
+get_winner(get_computer_choice(), get_user_choice())
