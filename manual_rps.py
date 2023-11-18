a22c54e50255bec9fc497890cecf61131c96ddd8
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-11-18 19:58:10 +0000

    refactor camera_rps and manual_rps

--- manual_rps.py
+++ manual_rps.py
@@ -1,45 +1,40 @@
 import random
 
+class RockPaperScissors:
+    def __init__(self) -> None:
+        self.options = ['rock', 'paper', 'scissors']
 
-def get_computer_choice():
-    options = ['rock', 'paper', 'scissors']
-    computer_choice = random.choice(options)
-    return computer_choice
+    def get_computer_choice(self):
+        return random.choice(self.options)
 
+    def get_user_choice(self):
+        user_input = input('Rock, Paper, Scissors? ').lower()
 
-def get_user_choice():
-    user_input = input('Rock, Paper, Scissors? ')
-    return user_input
+        while user_input not in self.options:
+            user_input = input('Invalid choice. Please choose Rock, Paper, or Scissors. ').lower()
 
+        return user_input
 
-def get_winner(computer_choice, user_choice):
-    comp = computer_choice.title()
-    user = user_choice.title()
-    if comp == 'Rock':
-        if user == 'Paper':
-            print('You won!')
-        elif user == 'Scissors':
-            print('You lost')
+    def get_winner(self, computer_choice, user_choice):
+        print(f'\nComputer picked {computer_choice.title()}')
+        print(f'You picked {user_choice.title()}')
+
+        if computer_choice == user_choice:
+            return '\nIt is a tie!'
+        if (computer_choice == 'rock' and user_choice == 'scissors') or \
+           (computer_choice == 'paper' and user_choice == 'rock') or \
+           (computer_choice == 'scissors' and user_choice == 'paper'):
+            return '\nComputer wins!'
         else:
-            print('It is a tie!')
-    elif comp == 'Paper':
-        if user == 'Rock':
-            print('You lost')
-        elif user == 'Scissors':
-            print('You won!')
-        else:
-            print('It is a tie!')
-    elif comp == 'Scissors':
-        if user == 'Rock':
-            print('You won!')
-        elif user == 'Paper':
-            print('You lost')
-        else:
-            print('It is a tie!')
+            return '\nYou win!'
+        
+    def play(self):
+        computer_choice = self.get_computer_choice()
+        user_choice = self.get_user_choice()
+        result = self.get_winner(computer_choice, user_choice)
 
+        print(result)    
 
-def play():
-    get_winner(get_computer_choice(), get_user_choice())
-    return
-
-play()
+# Create an instance of the game and play
+game = RockPaperScissors()
+game.play()
